83cd905e71f1e6b3e108f4e50c8627e0335a0f23
Facundo Decena <facundo.decena@gmail.com>
2020-03-13 12:52:49 -0300

    reformatted project

--- main.go
+++ main.go
@@ -53,9 +53,9 @@ func main() {
 				node = "0" + node
 			}
 			fmt.Printf(node)
-			if i < len(camino) - 1 {
+			if i < len(camino)-1 {
 				fmt.Print(" -> ")
-			} else{
+			} else {
 				fmt.Println()
 			}
 		}
@@ -98,25 +98,25 @@ func pathFinder(origen, destino int64, dimension int) []int64 {
 var clear map[string]func() //create a map for storing clear funcs
 
 func init() {
-    clear = make(map[string]func()) //Initialize it
-    clear["linux"] = func() { 
-        cmd := exec.Command("clear") //Linux example, its tested
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-    }
-    clear["windows"] = func() {
-        cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested 
-        cmd.Stdout = os.Stdout
-        cmd.Run()
-    }
+	clear = make(map[string]func()) //Initialize it
+	clear["linux"] = func() {
+		cmd := exec.Command("clear") //Linux example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
+	clear["windows"] = func() {
+		cmd := exec.Command("cmd", "/c", "cls") //Windows example, its tested
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	}
 }
 
 /*CallClear limpia la consola*/
 func CallClear() {
-    value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
-    if ok { //if we defined a clear func for that platform:
-        value()  //we execute it
-    } else { //unsupported platform
-        panic("Oops! Su sistema operativo no me dejo limpiar la pantalla")
-    }
+	value, ok := clear[runtime.GOOS] //runtime.GOOS -> linux, windows, darwin etc.
+	if ok {                          //if we defined a clear func for that platform:
+		value() //we execute it
+	} else { //unsupported platform
+		panic("Oops! Su sistema operativo no me dejo limpiar la pantalla")
+	}
 }
